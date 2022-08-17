5b007a6a8b83711dd6b77f63787c35f5e4045aa5
Hiago Prates <hiagohps360@gmail.com>
2022-08-16 22:36:50 -0300

    feat: helper modules

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
-import axios from 'axios';
+import Axios from 'axios';
+import { REACT_APP_API_URL } from '~/utils';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3333',
+export const request = Axios.create({
+  baseURL: REACT_APP_API_URL,
+  headers: {
+    accept: 'application/json',
+  },
+  timeout: 15000, // 15 seconds timeout
 });
 
-export default api;
+export default request;
---
+++ src/utils/modules/index.ts
@@ -0,0 +1,7 @@
+import ReactLoading from 'react-loading';
+import * as Toastify from 'react-toastify';
+
+export { Toastify, ReactLoading };
+
+export { observer, inject, MobXProviderContext, Provider } from 'mobx-react';
+export { default as styled } from 'styled-components';
